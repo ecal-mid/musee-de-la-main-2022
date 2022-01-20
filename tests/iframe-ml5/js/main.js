b3e97560785169a5ef93576f86f18ca97c0d22ad
Sébastien Matos <matos.sebastien@gmail.com>
2022-01-20 19:26:32 +0100

    fix user action for playing video
    make the video muted

--- tests/iframe-ml5/js/main.js
+++ tests/iframe-ml5/js/main.js
@@ -3,20 +3,28 @@ import { getWebcamStream } from "/utils/video.js";
 async function init() {
 
     const webcamStream = await getWebcamStream()
-
     const iframes = document.body.querySelectorAll('iframe')
+    const videos = []
 
-    //     (async () => {
+    // window.addEventListener('click', () => {
+    //     videos.forEach(video => {
+    //         video.play()
+    //     })
+    // })
 
     iframes.forEach(iframe => {
         iframe.onload = () => {
             const { stream, width, height } = webcamStream
             const { document } = iframe.contentWindow
+
             const video = document.body.querySelector('video')
+
             video.srcObject = stream
+            video.width = width
+            video.muted = true //! video must be muted for autoplay
             video.play()
-            // iframe.width = width
-            // iframe.height = height
+            video.height = height
+            videos.push(video)
         }
         iframe.src = './frames/iframe-posenet/index.html'
     })
