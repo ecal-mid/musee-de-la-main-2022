e53903a2b60a12799a2981b2bf1dd42ede7e3cc6
Sébastien Matos <matos.sebastien@gmail.com>
2022-03-08 23:29:29 +0100

    Fix photo crop melanie

--- static/projects/melanie/js/App.js
+++ static/projects/melanie/js/App.js
@@ -1,92 +1,91 @@
-const PICTURE_COUNTDOWN = 2;
-const STORAGE_LIMIT = 200;
+const PICTURE_COUNTDOWN = 2
+const STORAGE_LIMIT = 200
 
 const storage = new Storage({
   path: "melanie",
   origin: "http://localhost:1080",
-});
+})
 
 class App {
   constructor() { }
-  init({ canvas, video, pixelDensity = 1, mirrored = false }) {
+  init({ canvas, video, mirrored = false }) {
     // this.video_wrapper = document.getElementById("video");
-    this.canvas = canvas;
-    this.ctx = this.canvas.getContext("2d");
+    this.canvas = canvas
+    this.ctx = this.canvas.getContext("2d")
     this.mirrored = mirrored
 
     this.smoother = new MediaPipeSmoothPose({
       dampAmount: 0.1, // range ~1-10 [0 is fastest]
-    });
+    })
 
     // console.dir(this.video);
-    this.pixelDensity = pixelDensity;
-    this.video = video;
+    this.video = video
     // const { innerWidth, innerHeight } = window;
 
     // this.video.width = innerWidth;
     // this.video.height = innerHeight;
-    this.canvas.width = video.width * pixelDensity;
-    this.canvas.height = video.height * pixelDensity;
+    this.canvas.width = video.width
+    this.canvas.height = video.height
 
     // this.video_wrapper.appendChild(this.video);
     // this.video_wrapper.appendChild(this.canvas);
 
-    this.frameCount = 0;
-    this.circles = [];
-    this.texts = [];
-    this.dataURLs = [];
-    this.cachedPictures = {};
-    this.poses = [];
+    this.frameCount = 0
+    this.circles = []
+    this.texts = []
+    this.dataURLs = []
+    this.cachedPictures = {}
+    this.poses = []
     // this.mediaPipe = new MediaPipeClient()
     // this.loadPoseNetModel();
     // this.loadFaceDetection();
-    this.initMatter();
+    this.initMatter()
     //
-    this.floor = new Ground();
-    this.floor.groundLimit(this.MATTER, this.canvas.width, this.canvas.height);
+    this.floor = new Ground()
+    this.floor.groundLimit(this.MATTER, this.canvas.width, this.canvas.height)
 
-    this.initListeners();
+    this.initListeners()
 
-    this.isReadyFace = false;
+    this.isReadyFace = false
     this.detectionOptions = {
       withLandmarks: true,
       withDescriptors: false,
-    };
-    this.faceDetections;
-    this.detectFaces = [];
-    this.counter = 0;
-    this.cheese = 0;
-    this.currentImageIndex = 0;
-    this.info;
-    this.picIndex;
-    this.faceDetectionDuration = 0;
-    this.noDetectionDuration = 0;
-    this.changeState(PICTURE_COUNTDOWN);
+    }
+    this.faceDetections
+    this.detectFaces = []
+    this.counter = 0
+    this.cheese = 0
+    this.currentImageIndex = 0
+    this.info
+    this.picIndex
+    this.faceDetectionDuration = 0
+    this.noDetectionDuration = 0
+    this.changeState(PICTURE_COUNTDOWN)
 
 
-    const audioCtx = new AudioContext();
-    this.audio = new Audio("./sound/camera_sound.mp3");
+    const audioCtx = new AudioContext()
+    this.audio = new Audio("./sound/camera_sound.mp3")
 
     audioCtx.resume().then(() => {
-      const source = audioCtx.createMediaElementSource(this.audio);
-      source.connect(audioCtx.destination);
+      const source = audioCtx.createMediaElementSource(this.audio)
+      source.connect(audioCtx.destination)
     }).catch(e => console.log(e))
 
-    this.isReady = true;
+    this.isReady = true
   }
   onKeydown(e) {
     // this.getPicture();
     // console.log(e.key);
     switch (e.key) {
       case "Enter":
-        this.clearCircles();
-        break;
+        this.clearCircles()
+        break
     }
   }
 
   //firebase
   initListeners() {
-    document.addEventListener("keydown", this.onKeydown.bind(this));
+    document.addEventListener("keydown", this.onKeydown.bind(this))
 
     // this.getInfoPicture = firebase
     //   .database()
@@ -99,16 +98,16 @@ class App {
     storage
       .list("images")
       .then((list) => {
-        if (!Array.isArray(list)) return;
+        if (!Array.isArray(list)) return
 
         this.dataURLs = list.map((item) => {
-          return item.url;
-        });
+          return item.url
+        })
 
         //console.log(this.dataURLs)
         // console.log(this.dataURLs);
       })
-      .catch((e) => { });
+      .catch((e) => { })
 
     // console.log(b);
   }
@@ -121,7 +120,7 @@ class App {
       Body: Matter.Body,
       Bodies: Matter.Bodies,
       engine: Matter.Engine.create(),
-    };
+    }
     // this.floor = new Ground();
     // this.floor.groundLimit(this.MATTER);
   }
@@ -144,53 +143,62 @@ class App {
   // }
 
   onPose(pose) {
-    this.smoother.target(pose);
+    this.smoother.target(pose)
 
     if (!this.person && pose) {
-      const { width, height } = this.canvas;
-      this.person = new Person({ pose, MATTER: this.MATTER, width, height });
-      this.person.update(pose);
+      const { width, height } = this.canvas
+      this.person = new Person({ pose, MATTER: this.MATTER, width, height })
+      this.person.update(pose)
     }
 
-    this.poses.length = 0; //? clear array
-    if (pose) this.poses.push(pose);
+    this.poses.length = 0 //? clear array
+    if (pose) this.poses.push(pose)
   }
 
-  normalizeDensity(n) {
-    return n / this.pixelDensity;
-  }
+  async takeAndSendFace() {
 
-  async takeAndSendFace(boundary) {
-    const picIndex = Date.now();
+    const [pose] = this.poses
+    if (!pose) return
 
-    const { min, max } = boundary.body.bounds;
+    let { RIGHT_EAR, LEFT_EAR, NOSE } = pose
+    RIGHT_EAR = this.toScreen(RIGHT_EAR)
+    LEFT_EAR = this.toScreen(LEFT_EAR)
+    const w = Math.hypot(RIGHT_EAR.x - LEFT_EAR.x, RIGHT_EAR.y - LEFT_EAR.y) * 1.8
+    const center = this.toScreen(NOSE)
 
-    const margin = 10 * this.pixelDensity;
+    // ctx.save()
+    // ctx.strokeStyle = 'red'
+    // ctx.translate(center.x, center.y)
+    // ctx.translate(-w / 2, -w / 1.5)
+    // ctx.strokeRect(0, 0, w, w)
+    // ctx.restore()
 
-    const minX = this.normalizeDensity(min.x - margin);
-    const minY = this.normalizeDensity(min.y - margin);
-    const maxX = this.normalizeDensity(max.x + margin);
-    const maxY = this.normalizeDensity(max.y + margin);
+    const x = center.x - w / 2
+    const y = center.y - w / 1.5
 
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
+    const picIndex = Date.now()
 
-    const width = maxX - minX;
-    const height = maxY - minY;
+    const canvas = document.createElement("canvas")
+    const ctx = canvas.getContext("2d")
 
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = w
+    canvas.height = w
 
-    ctx.drawImage(this.video, minX, minY, width, height, 0, 0, width, height);
+    if (this.mirrored) {
+      ctx.scale(-1, 1)
+      ctx.translate(-w, 0)
+    }
 
-    const imageString = canvas.toDataURL("image/jpeg", 0.8);
+    ctx.drawImage(this.video, x, y, w, w, 0, 0, w, w)
+
+    const imageString = canvas.toDataURL("image/jpeg", 0.8)
     // console.log(imageString);
-    const image = new Image();
-    image.src = imageString;
+    const image = new Image()
+    image.src = imageString
     image.addEventListener('load', async () => {
-      image.onload = null;
-      const path = await storage.upload(`images/${picIndex}.jpg`, image);
-      this.dataURLs.push(path);
+      image.onload = null
+      const path = await storage.upload(`images/${picIndex}.jpg`, image)
+      this.dataURLs.push(path)
 
       while (this.dataURLs.length > STORAGE_LIMIT) {
         const firstElem = this.dataURLs.shift()
@@ -240,44 +248,44 @@ class App {
           null,
           this.MATTER
         )
-      );
-      this.frameCount = 0;
+      )
+      this.frameCount = 0
     }
   }
 
   async previousImage() {
-    if (this.dataURLs.length === 0) throw new Error("list empty");
+    if (this.dataURLs.length === 0) throw new Error("list empty")
     // this.imageToDisplay
     this.currentImageIndex = trueModulo(
       this.currentImageIndex - 1,
       this.dataURLs.length
-    );
+    )
 
     // console.log(this.currentImageIndex);
-    const src = this.dataURLs[this.currentImageIndex];
-    let image = this.cachedPictures[src];
+    const src = this.dataURLs[this.currentImageIndex]
+    let image = this.cachedPictures[src]
 
     if (!image) {
       image = await new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.onerror = (e) => reject(e);
-        img.src = src;
-      });
+        const img = new Image()
+        img.onload = () => resolve(img)
+        img.onerror = (e) => reject(e)
+        img.src = src
+      })
 
-      this.cachedPictures[src] = image;
+      this.cachedPictures[src] = image
     }
 
-    return image;
+    return image
   }
 
   drawBubbles() {
     // DRAW
     //! navigate through array in reverse, to delete circles while drawing
     for (let i = this.circles.length - 1; i >= 0; i--) {
-      const posXBall = this.circles[i].body.position.x;
-      const posYBall = this.circles[i].body.position.y;
-      const radius = this.circles[i].body.circleRadius;
+      const posXBall = this.circles[i].body.position.x
+      const posYBall = this.circles[i].body.position.y
+      const radius = this.circles[i].body.circleRadius
       //   let colorBall = this.circles[i].c;
       //   if (posYBall > 50) {
       //     colorBall = "#F00000";
@@ -291,26 +299,26 @@ class App {
         )
       ) {
         // console.log("HEAD TOUCHED");
-        this.circles[i].c = "black";
+        this.circles[i].c = "black"
         // this.circles[i].img = this.img
 
         if (!this.circles[i].img) {
           this.previousImage()
             .then((img) => {
-              this.circles[i].addImage(img);
+              this.circles[i].addImage(img)
             })
             .catch((e) => {
-              console.log("no image found!");
-            });
+              // console.log("no image found!")
+            })
         }
       }
 
-      this.circles[i].show(this.ctx);
+      this.circles[i].show(this.ctx)
 
       if (posYBall > this.canvas.height + 70) {
-        let index = i;
-        this.circles[i].removeFromWorld();
-        this.circles.splice(index, 1);
+        let index = i
+        this.circles[i].removeFromWorld()
+        this.circles.splice(index, 1)
       }
 
     }
@@ -318,34 +326,34 @@ class App {
 
   checkIfThereIsSomeone() {
     if (this.poses.length === 0) {
-      this.faceDetectionDuration = 0;
-      return;
+      this.faceDetectionDuration = 0
+      return
     }
-    this.faceDetectionDuration++;
+    this.faceDetectionDuration++
   }
 
   changeState(newState) {
-    this.state = newState;
+    this.state = newState
 
     switch (this.state) {
       case PICTURE_COUNTDOWN:
         {
-          this.clearPersonPhysic();
-          this.clearCircles(); //? wtf il en reste 29
+          this.clearPersonPhysic()
+          this.clearCircles() //? wtf il en reste 29
           // this.floor = new Ground();
           // this.floor.groundLimit(this.MATTER, this.canvas.width, this.canvas.height);
 
           // this.texts.forEach((text) => text.removeFromWorld(this.MATTER));
           // this.texts.length = 0;
         }
-        break;
+        break
 
       case 3:
         {
           // this.detectFaces.forEach((face) => {
-          this.takeAndSendFace(this.person.boundaries[NOSE]);
+          this.takeAndSendFace(this.person.boundaries[NOSE])
 
-          this.audio.play();
+          this.audio.play()
 
 
           // });
@@ -353,15 +361,15 @@ class App {
           // this.floor.groundLimit(this.MATTER);
           //facedetection duration = 0
         }
-        break;
+        break
     }
   }
 
   draw() {
-    this.ctx.fillStyle = "lightgrey";
+    this.ctx.fillStyle = "lightgrey"
 
-    const smoothedPose = this.smoother.smoothDamp();
-    const { width, height } = this.canvas;
+    const smoothedPose = this.smoother.smoothDamp()
+    const { width, height } = this.canvas
 
     this.ctx.save()
 
@@ -370,46 +378,46 @@ class App {
       this.ctx.translate(-width, 0)
     }
 
-    this.ctx.drawImage(this.video, 0, 0, width, height);
+    this.ctx.drawImage(this.video, 0, 0, width, height)
     this.ctx.restore()
 
-    this.MATTER.Engine.update(this.MATTER.engine); //! was in a state before
+    this.MATTER.Engine.update(this.MATTER.engine) //! was in a state before
     // console.log(this.MATTER.engine.world.bodies.length);
 
     switch (this.state) {
       case PICTURE_COUNTDOWN:
-        this.rainBubbles();
-        this.drawBubbles();
-        this.checkIfThereIsSomeone();
+        this.rainBubbles()
+        this.drawBubbles()
+        this.checkIfThereIsSomeone()
 
         if (this.faceDetectionDuration >= 150) {
-          this.faceDetectionDuration = 0;
-          this.changeState(3);
+          this.faceDetectionDuration = 0
+          this.changeState(3)
         }
 
-        break;
+        break
       case 3:
         // console.log("STATE3");
-        this.rainBubbles();
-        this.drawBubbles();
+        this.rainBubbles()
+        this.drawBubbles()
         //person
 
         if (this.person) {
-          this.person.update(smoothedPose);
+          this.person.update(smoothedPose)
           // this.person.show(this.ctx);
         }
 
         if (this.poses.length === 0) {
-          this.counter++;
+          this.counter++
 
           if (this.counter >= 100) {
-            console.log("NOBODY");
-            this.clearCircles();
-            this.clearPersonPhysic();
-            this.changeState(PICTURE_COUNTDOWN);
+            console.log("NOBODY")
+            this.clearCircles()
+            this.clearPersonPhysic()
+            this.changeState(PICTURE_COUNTDOWN)
           }
         } else {
-          this.counter = 0;
+          this.counter = 0
         }
         // if (this.person) {
         //   if (this.poses.length > 0) {
@@ -431,54 +439,64 @@ class App {
         //   }
         // }
 
-        break;
+        break
     }
 
     // this.MATTER.Engine.update(this.MATTER.engine); //! was in a state before
-    this.frameCount++;
+    this.frameCount++
 
     // this.floor.show()
 
     //
     const { ctx } = this
-    ctx.save();
-    ctx.translate(0, 0);
-    ctx.strokeStyle = 'red'
-    ctx.restore();
 
-    requestAnimationFrame(this.draw.bind(this));
+    requestAnimationFrame(this.draw.bind(this))
     // requestAnimationFrame(() => this.draw());
   }
 
+  toScreen(landmark) {
+    const { width, height } = this.canvas
+
+    let { x, y } = landmark
+
+    if (this.mirrored)
+      x = 1 - x
+
+    return {
+      x: x * width,
+      y: y * height
+    }
+  }
+
   clearCircles() {
-    this.circles.forEach((item) => item.removeFromWorld());
-    this.circles.length = 0;
+    this.circles.forEach((item) => item.removeFromWorld())
+    this.circles.length = 0
   }
   clearGroundPhysic() {
-    this.floor.removeFromWorld(this.MATTER);
+    this.floor.removeFromWorld(this.MATTER)
   }
   clearPersonPhysic() {
     if (this.person) {
-      this.person.destroy();
-      this.person = null;
+      this.person.destroy()
+      this.person = null
     }
   }
 }
 
 
-const mediaPipe = new MediaPipeClient();
-window.mediaPipe = mediaPipe; // global object mediaPipe
+const mediaPipe = new MediaPipeClient()
+window.mediaPipe = mediaPipe // global object mediaPipe
 
-const app = new App();
+const app = new App()
 
 mediaPipe.addEventListener("setup", () => {
-  const video = mediaPipe.video;
-  const canvas = document.querySelector(".main-canvas");
+  const video = mediaPipe.video
+  const canvas = document.querySelector(".main-canvas")
 
-  app.init({ canvas, video, pixelDensity: 1, mirrored: mediaPipe.mirrored });
-  app.draw();
+  app.init({ canvas, video, mirrored: mediaPipe.mirrored })
+  app.draw()
 
   mediaPipe.addEventListener("pose", (event) => {
-    app.onPose(event.data.skeleton);
-  });
-});
+    app.onPose(event.data.skeleton)
+  })
+})
